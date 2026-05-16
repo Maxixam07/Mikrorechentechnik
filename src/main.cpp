73f097de64905ef05d0954b662a3d55d0271671c
Wieland Schwarzenberg <wieland.schwarzenberg@gmx.de>
2026-05-16 16:40:38 +0200

    Beispielprogramm aus Anleitung reinkopiert

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,16 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
-
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+void setup()
+{
+  // Initialisiert Pin 14 (LED1) als output.
+  pinMode(14, OUTPUT);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+// LED1 blinkt im Zweisekundentakt
+void loop()
+{
+  digitalWrite(14, HIGH); // LED1 an
+  delay(1000);            // 1000 ms warten
+  digitalWrite(14, LOW);  // LED1 aus
+  delay(1000);            // 1000 ms warten
 }
