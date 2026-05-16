7d52cb66e3ee03ce2ee52c7b6c2970c456328f61
Wieland Schwarzenberg <wieland.schwarzenberg@gmx.de>
2026-05-16 18:05:15 +0200

    Aufgabe 5.3 implementiert

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,9 @@ int LED_1 = 14;           // Pin 14, Ausgangspin: Kann die Led1 an- und ausschal
 int LED_2 = 15;           // Pin 15, Ausgangspin: Kann die Led2 an- und ausschalten. Led leuchtet, wenn Pin auf high.
 
 bool lampe = false;
+int U_null_je_blink = 50; //Anzahl der Nulldurchgänge zwischen Änderung des Lampenzustandes (Blinkdauer)
+bool U_null_trans = false; //Flag, die false gesetzt wird, wenn U_null Low ist, also kurz vor dem Spannungsnulldurchgang
+int U_null_count = 0;     // Zähler für Spannungsnulldurchgänge 
 
 void setup()
 {
@@ -25,17 +28,24 @@ void setup()
 
 void loop()
 {
-  if (digitalRead(Taster_2) == HIGH)
+  if (digitalRead(U_null) == LOW) // U_null wird low, Spannungsnulldurchgang steht bevor
   {
-    lampe = false;
-    digitalWrite(iG_out, LOW);
-  }
-  else if (digitalRead(Taster_1) == HIGH)
-  {
-    lampe = true;
+    U_null_trans = true;
   }
 
-  if (lampe)
+  if (digitalRead(U_null) == HIGH && U_null_trans) //Wenn U_null zuvor LOW war und nun HIGH ist, ist der Spannungsnulldurchgang passiert
+  {
+    U_null_count++;         //Spannungsnulldurchgangzähler hochzählen
+    U_null_trans = false;   //Flag zurücksetzen
+  }
+
+  if (U_null_count >= U_null_je_blink) //Wenn angegebene Anzahl Spannungsnulldurchg. pro Blink erreicht sind
+  {
+    lampe = !lampe;                   //Lampenzustand ändern (blinken)
+    U_null_count = 0;                 //Nulldurchgangszähler zurücksetzen
+  }
+
+  if (lampe)                          //Handling des Triac-Inputs je nach bool lampe
   {
     digitalWrite(iG_out, HIGH);
   }
